9214fca610a459fecc752017dba8a57eeac2cc12
shivesh-anand <btech10218.21@bitmesra.ac.in>
2024-08-10 22:45:13 +0530

    more cookie changes

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
     },
   })
 );
