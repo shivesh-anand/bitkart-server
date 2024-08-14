2942e5e4fe1cffc07b6256f55a86905b10dd113c
shivesh-anand <btech10218.21@bitmesra.ac.in>
2024-08-14 22:46:34 +0530

    removed authMiddleware console logs

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -9,14 +9,14 @@ export const authMiddleware = (
   next: NextFunction
 ) => {
   const token = req.cookies.token;
-  console.log("token:", token);
+  //console.log("token:", token);
   if (!token) {
     return res.status(401).json({ message: "No token provided" });
   }
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET) as { id: string };
-    console.log("decoded:", decoded);
+    //console.log("decoded:", decoded);
     req.user = { id: decoded.id };
     next();
   } catch (error) {
